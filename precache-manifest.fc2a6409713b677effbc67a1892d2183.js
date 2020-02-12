ca32caef06e0b84fcfa1654328a397f3322f053b
Anton <antondeleeuw@gmail.com>
2020-02-12 09:02:24 +0100

    Updates

---
+++ precache-manifest.fc2a6409713b677effbc67a1892d2183.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30116e8c867461792312e4f82104a34a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0a87388c8748275b1020",
+    "url": "/monsters-rolodex/static/css/main.b99e5e6b.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a87388c8748275b1020",
+    "url": "/monsters-rolodex/static/js/main.d439cccb.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
